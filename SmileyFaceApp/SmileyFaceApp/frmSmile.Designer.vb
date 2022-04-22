3a6fd051008e7ab32780ebc2214c0a5417e940dc
MGAlysium <98758694+MGAlysium@users.noreply.github.com>
2022-04-22 09:52:40 -0500

    Add files via upload

---
+++ SmileyFaceApp/SmileyFaceApp/frmSmile.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSmile
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSmile))
+        Me.btnLD = New System.Windows.Forms.Button()
+        Me.btnLU = New System.Windows.Forms.Button()
+        Me.btnLL = New System.Windows.Forms.Button()
+        Me.btnLR = New System.Windows.Forms.Button()
+        Me.btnRR = New System.Windows.Forms.Button()
+        Me.btnRL = New System.Windows.Forms.Button()
+        Me.btnRU = New System.Windows.Forms.Button()
+        Me.btnRD = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnLD
+        '
+        Me.btnLD.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnLD.Enabled = False
+        Me.btnLD.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnLD.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLD.ForeColor = System.Drawing.Color.Teal
+        Me.btnLD.Location = New System.Drawing.Point(98, 372)
+        Me.btnLD.Name = "btnLD"
+        Me.btnLD.Size = New System.Drawing.Size(38, 38)
+        Me.btnLD.TabIndex = 1
+        Me.btnLD.Text = "v"
+        Me.btnLD.UseVisualStyleBackColor = False
+        '
+        'btnLU
+        '
+        Me.btnLU.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnLU.Enabled = False
+        Me.btnLU.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnLU.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLU.ForeColor = System.Drawing.Color.Teal
+        Me.btnLU.Location = New System.Drawing.Point(98, 290)
+        Me.btnLU.Name = "btnLU"
+        Me.btnLU.Size = New System.Drawing.Size(38, 38)
+        Me.btnLU.TabIndex = 2
+        Me.btnLU.Text = "^"
+        Me.btnLU.UseVisualStyleBackColor = False
+        '
+        'btnLL
+        '
+        Me.btnLL.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnLL.Enabled = False
+        Me.btnLL.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnLL.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLL.ForeColor = System.Drawing.Color.Teal
+        Me.btnLL.Location = New System.Drawing.Point(54, 332)
+        Me.btnLL.Name = "btnLL"
+        Me.btnLL.Size = New System.Drawing.Size(38, 38)
+        Me.btnLL.TabIndex = 3
+        Me.btnLL.Text = "<"
+        Me.btnLL.UseVisualStyleBackColor = False
+        '
+        'btnLR
+        '
+        Me.btnLR.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnLR.Enabled = False
+        Me.btnLR.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnLR.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLR.ForeColor = System.Drawing.Color.Teal
+        Me.btnLR.Location = New System.Drawing.Point(142, 332)
+        Me.btnLR.Name = "btnLR"
+        Me.btnLR.Size = New System.Drawing.Size(38, 38)
+        Me.btnLR.TabIndex = 4
+        Me.btnLR.Text = ">"
+        Me.btnLR.UseVisualStyleBackColor = False
+        '
+        'btnRR
+        '
+        Me.btnRR.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnRR.Enabled = False
+        Me.btnRR.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnRR.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRR.ForeColor = System.Drawing.Color.Teal
+        Me.btnRR.Location = New System.Drawing.Point(372, 332)
+        Me.btnRR.Name = "btnRR"
+        Me.btnRR.Size = New System.Drawing.Size(38, 38)
+        Me.btnRR.TabIndex = 8
+        Me.btnRR.Text = ">"
+        Me.btnRR.UseVisualStyleBackColor = False
+        '
+        'btnRL
+        '
+        Me.btnRL.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnRL.Enabled = False
+        Me.btnRL.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnRL.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRL.ForeColor = System.Drawing.Color.Teal
+        Me.btnRL.Location = New System.Drawing.Point(284, 332)
+        Me.btnRL.Name = "btnRL"
+        Me.btnRL.Size = New System.Drawing.Size(38, 38)
+        Me.btnRL.TabIndex = 7
+        Me.btnRL.Text = "<"
+        Me.btnRL.UseVisualStyleBackColor = False
+        '
+        'btnRU
+        '
+        Me.btnRU.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnRU.Enabled = False
+        Me.btnRU.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnRU.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRU.ForeColor = System.Drawing.Color.Teal
+        Me.btnRU.Location = New System.Drawing.Point(328, 290)
+        Me.btnRU.Name = "btnRU"
+        Me.btnRU.Size = New System.Drawing.Size(38, 38)
+        Me.btnRU.TabIndex = 6
+        Me.btnRU.Text = "^"
+        Me.btnRU.UseVisualStyleBackColor = False
+        '
+        'btnRD
+        '
+        Me.btnRD.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnRD.Enabled = False
+        Me.btnRD.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnRD.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRD.ForeColor = System.Drawing.Color.Teal
+        Me.btnRD.Location = New System.Drawing.Point(328, 372)
+        Me.btnRD.Name = "btnRD"
+        Me.btnRD.Size = New System.Drawing.Size(38, 38)
+        Me.btnRD.TabIndex = 5
+        Me.btnRD.Text = "v"
+        Me.btnRD.UseVisualStyleBackColor = False
+        '
+        'btnStart
+        '
+        Me.btnStart.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnStart.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStart.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnStart.Location = New System.Drawing.Point(178, 290)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(109, 29)
+        Me.btnStart.TabIndex = 9
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = False
+        '
+        'frmSmile
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(464, 441)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.btnRR)
+        Me.Controls.Add(Me.btnRL)
+        Me.Controls.Add(Me.btnRU)
+        Me.Controls.Add(Me.btnRD)
+        Me.Controls.Add(Me.btnLR)
+        Me.Controls.Add(Me.btnLL)
+        Me.Controls.Add(Me.btnLU)
+        Me.Controls.Add(Me.btnLD)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.Teal
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "frmSmile"
+        Me.Text = "Smiley Face"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnLD As System.Windows.Forms.Button
+    Friend WithEvents btnLU As System.Windows.Forms.Button
+    Friend WithEvents btnLL As System.Windows.Forms.Button
+    Friend WithEvents btnLR As System.Windows.Forms.Button
+    Friend WithEvents btnRR As System.Windows.Forms.Button
+    Friend WithEvents btnRL As System.Windows.Forms.Button
+    Friend WithEvents btnRU As System.Windows.Forms.Button
+    Friend WithEvents btnRD As System.Windows.Forms.Button
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+
+End Class
---
+++ SmileyFaceApp/SmileyFaceApp/frmSmile.vb
@@ -0,0 +1,78 @@
+﻿Public Class frmSmile
+    Dim pupilLX As Integer = 215
+    Dim pupilLY As Integer = 84
+    Dim pupilRX As Integer = 254
+    Dim pupilRY As Integer = 84
+
+    Dim pen As New Pen(Brushes.Black, 2)
+    Dim pnt As Graphics
+
+    Private Sub DrawFace()
+        pnt = Me.CreateGraphics
+        pnt.Clear(Color.Silver)
+        pnt.FillEllipse(Brushes.Yellow, 165, 50, 150, 150)
+        pnt.DrawEllipse(pen, 165, 50, 150, 150)
+        pnt.FillEllipse(Brushes.White, 200, 70, 30, 50)
+        pnt.DrawEllipse(pen, 200, 70, 30, 50)
+        pnt.FillEllipse(Brushes.White, 255, 70, 30, 50)
+        pnt.DrawEllipse(pen, 255, 70, 30, 50)
+        pnt.FillEllipse(Brushes.Purple, pupilLX, pupilLY, 15, 25)
+        pnt.DrawEllipse(pen, pupilLX, pupilLY, 15, 25)
+        pnt.FillEllipse(Brushes.Purple, pupilRX, pupilRY, 15, 25)
+        pnt.DrawEllipse(pen, pupilRX, pupilRY, 15, 25)
+        pnt.DrawArc(pen, 200, 115, 85, 50, 0, 180)
+    End Sub
+
+    Private Sub btnLL_Click(sender As Object, e As EventArgs) Handles btnLL.Click
+        pupilLX -= 2
+        DrawFace()
+    End Sub
+
+    Private Sub btnLU_Click(sender As Object, e As EventArgs) Handles btnLU.Click
+        pupilLY -= 2
+        DrawFace()
+    End Sub
+
+    Private Sub btnLR_Click(sender As Object, e As EventArgs) Handles btnLR.Click
+        pupilLX += 2
+        DrawFace()
+    End Sub
+
+    Private Sub btnLD_Click(sender As Object, e As EventArgs) Handles btnLD.Click
+        pupilLY += 2
+        DrawFace()
+    End Sub
+
+    Private Sub btnRL_Click(sender As Object, e As EventArgs) Handles btnRL.Click
+        pupilRX -= 2
+        DrawFace()
+    End Sub
+
+    Private Sub btnRU_Click(sender As Object, e As EventArgs) Handles btnRU.Click
+        pupilRY -= 2
+        DrawFace()
+    End Sub
+
+    Private Sub btnRR_Click(sender As Object, e As EventArgs) Handles btnRR.Click
+        pupilRX += 2
+        DrawFace()
+    End Sub
+
+    Private Sub btnRD_Click(sender As Object, e As EventArgs) Handles btnRD.Click
+        pupilRY += 2
+        DrawFace()
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        DrawFace()
+        btnStart.Visible = False
+        btnLD.Enabled = True
+        btnRD.Enabled = True
+        btnLL.Enabled = True
+        btnRL.Enabled = True
+        btnLU.Enabled = True
+        btnRU.Enabled = True
+        btnLR.Enabled = True
+        btnRR.Enabled = True
+    End Sub
+End Class
